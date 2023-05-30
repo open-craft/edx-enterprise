8c499ec8a63aca04fb266b4cb2507c98589e483a
Arunmozhi <tecoholic@users.noreply.github.com>
2023-05-30 12:55:05 +0530

    feat: enable enrolling in invite_only courses via manage learners UI
    When enrollment of users to courses marked as "Invite Only" is attempted
    from the "Manage Learners" page in the Admin, it results in an error due
    to EnrollmentClosedError. This commit, adds handles that scenario by
    setting "force_enrollment" to True and creating a CourseEnrollmentAllowed
    object if the users with the specified emails didn't exist.

--- enterprise/admin/forms.py
+++ enterprise/admin/forms.py
@@ -67,6 +67,11 @@ class ManageLearnersForm(forms.Form):
         label=_("Enroll these learners in this course"), required=False,
         help_text=_("To enroll learners in a course, enter a course ID."),
     )
+    force_enrollment = forms.BooleanField(
+        label=_("Force Enrollment"),
+        help_text=_("The selected course is 'Invite Only'. Only staff can enroll learners to this course."),
+        required=False,
+    )
     course_mode = forms.ChoiceField(
         label=_("Course enrollment track"), required=False,
         choices=BLANK_CHOICE_DASH + [
@@ -128,6 +133,7 @@ class ManageLearnersForm(forms.Form):
         REASON = "reason"
         SALES_FORCE_ID = "sales_force_id"
         DISCOUNT = "discount"
+        FORCE_ENROLLMENT = "force_enrollment"
 
     class CsvColumns:
         """
