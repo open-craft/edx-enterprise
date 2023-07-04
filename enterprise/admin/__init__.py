fa66def8c8501299d34285792e2759225fcae7e2
Arunmozhi <arunmozhi@opencraft.com>
2023-07-04 13:44:14 +0530

    feat: adds attribute to hide course prices when zero
    This adds a new attribute `hide_course_price_when_zero` to the
    EnterpriseCustomer model, which will hide the pricing information
    from the enrollment page when the final price of a premium course
    mode is Zero.
    
    fix: removed unused variable from template context
    
    fix: quality issue and add the new attribute to test
    
    fix: update the help text in the db migration
    
    feat: adds final price to logging
    
    fix: change the attribute in the utils test causing test failure
    
    refactor: renamed the migration file
    
    chore: rebase on master and update db migration
    
    fix: remove empty lines causing linting failure
    
    fix: remove the empty line in tests file
    
    -------------------------
    
    Additionally includes this fix:
    https://tasks.opencraft.com/browse/BB-7541?focusedCommentId=282910&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-282910

--- enterprise/admin/__init__.py
+++ enterprise/admin/__init__.py
@@ -202,7 +202,7 @@ class EnterpriseCustomerAdmin(DjangoObjectActions, SimpleHistoryAdmin):
         ('Integration and learning platform settings', {
             'fields': ('enable_portal_lms_configurations_screen', 'enable_portal_saml_configuration_screen',
                        'enable_slug_login', 'replace_sensitive_sso_username', 'hide_course_original_price',
-                       'enable_generation_of_api_credentials')
+                       'hide_course_price_when_zero', 'enable_generation_of_api_credentials')
         }),
         ('Recommended default settings for all enterprise customers', {
             'fields': ('site', 'customer_type', 'enable_learner_portal',
