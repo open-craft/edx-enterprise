0069d2c3163ce3b9e0a35cbf3c17af3836b40268
Arunmozhi <arun@arunmozhi.in>
2023-07-14 14:48:58 +0530

    temp: add utility function to add CEA objects
    feat: adds allow invite only enrollment flag
    
    feat: adds allow invite only enrollment flag
    
    feat: create cea only when customer has invite-only enrollments enabled
    
    fix: simplify the cea creation logic, update tests
    
    fix: remove a stray empty line
    
    feat: adds the invite-only flag to customer admin
    
    fix: typo in the fuction docstring
    
    Co-authored-by: Piotr Surowiec <piotr@surowiec.it>
    
    refactor: convert the user consent flow handler method to static
    
    fix: move migrations to avoid conflicts
    
    feat: add typing to the ensure cea utility method
    
    Revert "feat: add typing to the ensure cea utility method"
    
    This reverts commit b6b2f2590e23263a2671b469f139c2c12abcac3b.
    
    refactor: rename the migration with a custom name instead of the auto one
    
    fix: remove typing that breaks causes import failures
    
    chore: move the DB migration to latest version
    
    -----------------------
    
    Additionally includes this fix:
    https://tasks.opencraft.com/browse/BB-7619?focusedCommentId=282911&page=com.atlassian.jira.plugin.system.issuetabpanels%3Acomment-tabpanel#comment-282911

--- enterprise/admin/__init__.py
+++ enterprise/admin/__init__.py
@@ -202,7 +202,8 @@ class EnterpriseCustomerAdmin(DjangoObjectActions, SimpleHistoryAdmin):
         ('Integration and learning platform settings', {
             'fields': ('enable_portal_lms_configurations_screen', 'enable_portal_saml_configuration_screen',
                        'enable_slug_login', 'replace_sensitive_sso_username', 'hide_course_original_price',
-                       'hide_course_price_when_zero', 'enable_generation_of_api_credentials')
+                       'hide_course_price_when_zero', 'allow_enrollment_in_invite_only_courses',
+                       'enable_generation_of_api_credentials')
         }),
         ('Recommended default settings for all enterprise customers', {
             'fields': ('site', 'customer_type', 'enable_learner_portal',
